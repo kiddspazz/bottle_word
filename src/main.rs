b437167bcb90497c039a1d9385013af3b68bd09d
kiddspazz <travis@travish.com>
2019-11-25 20:30:40 -0700

    match statements ftw

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn _main() {
     for x in (1..=99).rev() {
         let bottle_word = if x > 1 {"bottles"} else {"bottle"};
         let next_bottle_word = if x > 2 {"bottles"} else {"bottle"};
@@ -20,4 +20,15 @@ fn main() {
     }
     println!("No more bottles of beer on the wall, no more bottles of beer.");
     println!("Go to the store and buy some more, 99 bottle_word of beer on the wall.");
+}
+
+fn main() {
+    for x in (0..=99).rev() {
+        match x {
+            2 => println!("this is 2"),
+            1 => println!("this is 1"),
+            0 => println!("0 woohoo!"),
+            _ => println!("All the other numbers! BOOM BABY")
+        }
+    }
 }
